a52cd5c6638196237aa1a93f00460af3db0e772d
hsilvioviana <hsilvioviana@hotmail.com>
2021-04-22 16:31:25 -0300

    Botao para adicionar na pokedex

--- pokedex/src/pages/Details/index.js
+++ pokedex/src/pages/Details/index.js
@@ -1,14 +1,13 @@
 import { useContext } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-import { ContextoPokemon } from '../../context'
-import { goToHome, goToPokedex } from '../../routes/coordinator'
+import { GlobalStateContext } from '../../global/GlobalStateContext'
 import Status from './Status/index'
 
 export default function Details() {
 
     const history = useHistory()
     const params = useParams()
-    const pokemons = useContext(ContextoPokemon)
+    const pokemons = useContext(GlobalStateContext).pokemons
     const pokemon = pokemons.find(pokemon => (pokemon.name === params.id || pokemon.id === Number(params.id)))
 
     return <div>
--- pokedex/src/pages/Pokedex/index.js
+++ pokedex/src/pages/Pokedex/index.js
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom'
-import { goToHome, goToPokedex, goToDetails} from '../routes/coordinator'
+import { goToHome, goToPokedex } from '../../routes/coordinator'
+import React from 'react'
+import PokemonCard from '../../components/PokemonCard'
 
 export default function Pokedex() {
     const history = useHistory()
@@ -7,5 +9,6 @@ export default function Pokedex() {
         <h1>Pokedex</h1>
         <button onClick={() => goToHome(history)}>Home</button>
         <button onClick={() => goToPokedex(history)}>Pokedex</button>
+        <PokemonCard history={history} home={false}/>
     </div>)
 }
