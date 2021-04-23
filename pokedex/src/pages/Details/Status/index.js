c4db0f3b503ad848bc9e5adcc7fc8646c5e42f45
Eduardo Silva <eduardo.sf.1992@gmail.com>
2021-04-23 11:22:53 -0300

    pagina details estilizada

--- pokedex/src/pages/Details/Status/index.js
+++ pokedex/src/pages/Details/Status/index.js
@@ -15,73 +15,62 @@ export default function Details(props) {
         <c.DetailsContainer>
             <c.BoxLeft>
                 <c.Photo>
-                    <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="" title="" /> 
+                    <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="" title="" />
                 </c.Photo>
             </c.BoxLeft>
             <c.BoxRight>
                 <c.PokedexData>
-                    <h1>Dados do Pokemons:</h1>
+                    <h1>Dados do Pokemon:</h1>
                     <ul>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <c.SkillsDetails>
+                                <span>Types:</span>
+                                <span className="fire">Grama</span>
+                                <span className="grass">Poison</span>
+                            </c.SkillsDetails>
                         </li>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
-                        </li>
-                        <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
-                        </li>
-                        <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
-                        </li>
-                        <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>Moves:</span>
+                            <span>mega-punch</span>
+                            <span>pay-day</span>
+                            <span>thunder-punch</span>
+                            <span>slam</span>
+                            <span>mega-kick</span>
                         </li>
                     </ul>
                 </c.PokedexData>
                 <c.PokedexStatistic>
-                <h1>Estatísticas:</h1>
+                    <h1>Estatísticas:</h1>
                     <ul>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>HP:</span>
+                            <span>100</span>
                         </li>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>Attack:</span>
+                            <span>100</span>
                         </li>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>Defense:</span>
+                            <span>100</span>
                         </li>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>Special-Attack:</span>
+                            <span>100</span>
                         </li>
                         <li>
-                            <span>Tipo:</span>
-                            <span>Grama</span>
-                            <span>Poison</span>
+                            <span>Special-Defense:</span>
+                            <span>100</span>
+                        </li>
+                        <li>
+                            <span>Speed:</span>
+                            <span>100</span>
                         </li>
                     </ul>
                 </c.PokedexStatistic>
             </c.BoxRight>
         </c.DetailsContainer>
-        {/* <h1>{props.pokemon.name}</h1>
+        <h1>{props.pokemon.name}</h1>
         <img src={`https://raw.githubusercontent.com/geekygreek7/animated-pokemon-gifs/master/${props.pokemon.id}.gif`} alt="Foto do Pokemon" />
         {!props.loading && props.pokemon.stats.map((pokemon) => {
             return <h1 key={pokemon.stat.name}>{pokemon.stat.name}: {pokemon.base_stat}</h1>
@@ -94,6 +83,6 @@ export default function Details(props) {
         <h3>Types:</h3>
         {!props.loading && props.pokemon.types.slice(0, 5).map((pokemon) => {
             return <p>{pokemon.type.name}</p>
-        })} */}
+        })}
     </div>)
 }
--- pokedex/src/pages/Details/Status/style.js
+++ pokedex/src/pages/Details/Status/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'   
 import { Container } from '../../../styles/styles'
+import { Skills } from '../../../styles/styles'
 
 export const DetailsContainer = styled(Container)`
     display: flex;
@@ -32,13 +33,52 @@ export const BoxLeft = styled.div`
 export const BoxRight = styled.div`
     flex-grow: 1;
     display: flex;
-    flex-flow: row nowrap;
-    justify-content: center;
+    flex-flow: column nowrap;
+    margin-left: 20px;
+    & ul > * {
+        padding: 6px 0;
+    }
+    & h1 {
+        color: #999;
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+    }
+    & > * ul li {
+        padding: 8px 0;
+        border-bottom: 1px solid #ccc;
+    }
+
+    & > :first-child li:first-of-type {
+        padding-bottom: 14px;
+    }
 
 `
 export const PokedexData = styled.div`
+    & li:nth-child(2) > span:nth-child(n+2) {
+        padding-left: 5px;
+        margin-left: 4px;
+        border-left: 1px solid #aaa;
+        color: #777;
+    }
+    & li:nth-child(2) > span:nth-child(2) {
+        border: 0;
+    }
 `
 
 export const PokedexStatistic = styled.div`
-    margin-left: 20px;
+    & > ul li {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
+    }
+    margin-top: 20px;
+`
+
+export const SkillsDetails = styled(Skills)`
+    margin-top: 0;
+    & > :first-child {
+        font-size: 1em;
+        color: #222;
+        padding: 0;
+    }
 `
